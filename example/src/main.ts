6ba2be62ff2479e67f2ce85220050b0b12604733
Mirko Brombin <send@mirko.pm>
2024-04-10 14:53:03 +0200

    Add BrowserEngine example

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,11 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VNT from 'vnt/plugin/vnt'
+
+/* 
+EXAMPLE: Using Tauri Engine
+*/
 import TauriEngine from 'vnt/lib/engines/Tauri'
 import { Window } from '@tauri-apps/api/window'
 
 const app = createApp(App)
-
 app.use(VNT, new TauriEngine(Window.getCurrent()))
+
+/*
+EXAMPLE: Using Browser Engine
+
+import BrowserEngine from 'vnt/lib/engines/Browser'
+
+const app = createApp(App)
+app.use(VNT, new BrowserEngine())
+*/
 
 app.mount('#app')

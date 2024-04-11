73f91fbe20a9bf2dff9dc8a1bc67eca0e0e851db
Mirko Brombin <send@mirko.pm>
2024-04-11 21:17:10 +0200

    Fix inputType return type in BaseField.vue

--- widgets/BaseField.vue
+++ widgets/BaseField.vue
@@ -74,7 +74,7 @@ export default defineComponent({
                 this.isActive = false;
             }
         },
-        inputType() {
+        inputType(): string {
             return this.type;
         },
     },
